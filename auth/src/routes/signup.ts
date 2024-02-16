f5d360d920814a5d9f7eb9aaa5b75153a066bab0
harumaki <115347054+Amoonguses1@users.noreply.github.com>
2024-02-16 18:37:13 +0900

    Implement signin, signout and signup systems

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middleware/validate-request';
 
 const router = express.Router();
 
@@ -15,13 +17,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters'),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
@@ -34,6 +31,20 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // generate JWT
+    const userJWT = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // store it on session object
+    req.session = {
+      jwt: userJWT,
+    };
 
     res.status(201).send(user);
   }
