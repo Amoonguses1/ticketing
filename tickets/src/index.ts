6dd4cb1c088515846518ec2d43621cd0d864a230
harumaki <115347054+Amoonguses1@users.noreply.github.com>
2024-02-24 04:03:23 +0900

    Implement basic ticket service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -9,14 +9,16 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
+
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('Connected to mongoDB');
+    console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
   }
+
   app.listen(3000, () => {
-    console.log('Listening on port 3000!');
+    console.log('Listening on port 3000!!!!!!!!');
   });
 };
 
