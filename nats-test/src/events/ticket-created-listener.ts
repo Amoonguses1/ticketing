7c36c01150b784ba88e4548fa8ecc4eaa422535d
harumaki <115347054+Amoonguses1@users.noreply.github.com>
2024-03-01 22:52:03 +0900

    Created skelton in nats-streaming for ticket creating and updating

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,18 @@
+import nats from 'node-nats-streaming';
+import { Listener } from './base-listener';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
+  queueGroupName = 'payments-service';
+
+  onMessage(data: TicketCreatedEvent['data'], msg: nats.Message): void {
+    console.log('Event data', data);
+
+    console.log(data.id);
+    console.log(data.price);
+
+    msg.ack();
+  }
+}
