4bf9915f08891bdeefe413801228d4cafbe50934
harumaki <115347054+Amoonguses1@users.noreply.github.com>
2024-03-02 14:29:53 +0900

    Error occurred in TicketCreatedPublisher class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,6 +1,6 @@
 import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
-import { TicketCreatedListener } from '../../common/src/events/ticket-created-listener';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
